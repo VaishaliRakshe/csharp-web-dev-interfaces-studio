e6f8afbb791f45dcd58478bb2d2f37f496be2928
VaishaliRakshe <vaishalijadhav000@gmail.com>
2021-12-06 14:36:52 -0600

    update

--- SpinningDiscs/CD.cs
+++ SpinningDiscs/CD.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace SpinningDiscs
 {
-    public class CD
+    public class CD : BaseDisc
     {
+        public CD(int storageCapacity, string name, double discSpeed, List<string> data) : base(storageCapacity, name, discSpeed, data);
+        {
+    }
+        public static void Spindisc()
+        {
+            Console.WriteLine("A CD spin at the rate of 200-500rpm");
+        }
         // TODO: Implement your custom interface.
 
         // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
